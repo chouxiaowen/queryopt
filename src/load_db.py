ab395c57451a7fe215b235691d5d01990f94e276
Liwen Sun <chouxiaowen@gmail.com>
2013-03-01 01:13:09 +0000

    src/load_db.py

--- src/load_db.py
+++ src/load_db.py
@@ -1,6 +1,7 @@
+import sys
+import os
 import preprocess as prep
 import psycopg2
-
 
 # Create a schema string by parsing 'header_file'
 def create_schema(header_file):
@@ -16,7 +17,7 @@ def create_schema(header_file):
 # if mode='individual', create one table for each file
 def create_table(data_path, header_file, table_name, db_name, mode):
   # connect to db
-  conn = psycopg2.connect("dbname=%s port=11111" % dbname)
+  conn = psycopg2.connect("dbname=%s port=11111" % db_name)
   conn.set_isolation_level(0)
   cur = conn.cursor()
 
@@ -24,22 +25,29 @@ def create_table(data_path, header_file, table_name, db_name, mode):
 
   if mode == 'individual':
     for f in prep.gen_file_list(data_path):
-      table_name = table_name + f[f.rfind('/')+1:]
-      cur.execute("DROP TABLE IF EXISTS %s;" % table_name)
-      cur.execute("CREATE TABLE %s %s;" % (table_name, schema))
-      cur.execute("COPY %s FROM '%s' WITH (FORMAT CSV DELIMITER '\t');" % (table_name, f))
+      file_name = f[f.rfind('/')+1:]
+      cur_path = f[:f.rfind('/')]
+      full_path = os.path.abspath(cur_path)
+
+      t_name = table_name + '_' + file_name 
+      cur.execute("DROP TABLE IF EXISTS %s;" % t_name)
+      cur.execute("CREATE TABLE %s (%s);" % (t_name, schema))
+      cur.execute("COPY %s FROM '%s';" % (t_name, full_path + '/' + file_name))
 
   elif mode == 'partitioned':
   # create master table
-    master_table = table_name
-    cur.execute("DROP TABLE IF EXISTS %s;" % master_table)
-    cur.execute("CREATE TABLE %s %s;" % (master_table, schema))
-
+    cur.execute("DROP TABLE IF EXISTS %s CASCADE;" % table_name)
+    cur.execute("CREATE TABLE %s (%s);" % (table_name, schema))
+    
     for f in prep.gen_file_list(data_path):
-      table_name = f[f.rfind('/')+1:]
-      cur.execute("DROP TABLE IF EXISTS %s;" % f)
-      cur.execute("CREATE TABLE %s () INHERITS (%s);" % (table_name, master_table))
-      cur.execute("COPY %s FROM '%s' WITH (FORMAT CSV DELIMITER '\t');" % (table_name, f))
+      file_name = f[f.rfind('/')+1:]
+      cur_path = f[:f.rfind('/')]
+      full_path = os.path.abspath(cur_path)
+
+      t_name = table_name + '_' + file_name 
+      cur.execute("DROP TABLE IF EXISTS %s;" % t_name)
+      cur.execute("CREATE TABLE %s () INHERITS (%s);" % (t_name, table_name))
+      cur.execute("COPY %s FROM '%s' ;" % (t_name, full_path + '/' + file_name))
 
   else:
     print 'unknown mode: %s' % mode
@@ -54,7 +62,7 @@ def create_db(dbname):
   conn.set_isolation_level(0)
 
   cur = conn.cursor()
- cur.execute("DROP DATABASE IF EXISTS %s;" % dbname)
+  cur.execute("DROP DATABASE IF EXISTS %s;" % dbname)
   cur.execute("CREATE DATABASE %s;" % dbname)
   cur.close()
   conn.close()
